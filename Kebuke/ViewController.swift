4d7cc0d629fc1337646319ce83c0a28ea3c0c769
Rabbit <labcgood44@gmail.com>
2024-07-18 15:39:47 +0800

    Initial Commit

---
+++ Kebuke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kebuke
+//
+//  Created by Labe on 2024/7/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
